713d5b420ab4c9dd75f63f64a38547f424c99c63
Thiago Renan <thiagorenan1108@gmail.com>
2025-06-28 21:36:40 -0300

    Criando INSERT ao banco de dados

--- java-jdbc/src/main/java/dao/UserJdbcDao.java
+++ java-jdbc/src/main/java/dao/UserJdbcDao.java
@@ -1,14 +1,32 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 import conexaojdbc.SingleConnection;
+import model.UserJavaJdbc;
 
-public class UserJdbcDao {
+public class UserJdbcDao {// Dao -> Classe onde cria as operações de insert usando o objedo model
 
 	private Connection connection;
+
 	
-	public UserJdbcDao(Connection connection) {
+	public UserJdbcDao() {
 		connection = SingleConnection.getConnection();
+	}
+	
+	
+	public void salvar(UserJavaJdbc user) {
+		try {
+		String sql = "insert into userjavajdbc (id, nome, email) values (?,?,?)";
+		PreparedStatement insert = connection.prepareStatement(sql); //Realiza o insert
+		insert.setLong(1, user.getId());
+		insert.setString(2, user.getNome());
+		insert.setString(3, user.getEmail());
+		insert.execute();
+		connection.commit();// Salvar no banco de dados
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- java-jdbc/src/test/java/java_jdbc/java_jdbc/TesteBancoJdbc.java
+++ java-jdbc/src/test/java/java_jdbc/java_jdbc/TesteBancoJdbc.java
@@ -3,12 +3,22 @@ package java_jdbc.java_jdbc;
 import org.junit.jupiter.api.Test;
 
 import conexaojdbc.SingleConnection;
+import dao.UserJdbcDao;
+import model.UserJavaJdbc;
 
 public class TesteBancoJdbc {
 	
 	@Test
 	public void initBanco() {
-		SingleConnection.getConnection();
+		UserJdbcDao userJdbcDao = new UserJdbcDao();
+		
+		UserJavaJdbc userJavaJdbc = new UserJavaJdbc();
+		
+		userJavaJdbc.setId(5L);
+		userJavaJdbc.setNome("Charles");
+		userJavaJdbc.setEmail("charles@gmailcom");
+		
+		userJdbcDao.salvar(userJavaJdbc);
 	}
 
 }
